293d90696012965253e2bf695e0e0a415745ca04
Pavel <n9032692530@gmail.com>
2022-09-05 17:59:04 +0300

    Ex002

--- Tasks/Ex002/Program.cs
+++ Tasks/Ex002/Program.cs
@@ -5,10 +5,17 @@
 
 int Sum(string number)
     {
-        int[] numbers = Array.ConvertAll(text.Split(),int.Parse);
+        int S = 0;
+        int[] numbers = number.ToString().Select(c=>(int)char.GetNumericValue(c)).ToArray();
+        for(int i = 0; i<number.Length; i++)
+            S=S+numbers[i]; 
+            
+        return S;
     }
 
 Console.WriteLine("Введите число - ");
 int num=Convert.ToInt32(Console.ReadLine());
-string Text=Convert.ToString(n);
-Sum(Text);
+if(num<0)
+    num=num*(-1);
+string Text=Convert.ToString(num);
+Console.WriteLine(Sum(Text));
