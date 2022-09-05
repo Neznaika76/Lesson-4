53d098fed24e88c10189d1b9c597e51469426226
Pavel <n9032692530@gmail.com>
2022-09-05 20:51:00 +0300

    Tasks 39

---
+++ Tasks/Ex007/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 39: Задайте массив вещественных чисел. 
+// Поменяйте местами элементы массива.
+// [3 7 22 2 78] -> 76
+
+void Sum(double[] Arr)
+    {
+        double Per = Arr[0];
+        for(int i=0, j =Arr.Length-1; i<Arr.Length/2; i++,j--)
+            {
+                Per = Arr[i];
+                Arr[i]=Arr[j];
+                Arr[j]=Per;
+            }
+            //Console.WriteLine("Максимум " + max);
+            //Console.WriteLine("Минимум " + min);
+            //Console.WriteLine("Разность между максимальным и минимальным элементом равна " + max);
+    }
+
+Console.Write("Введите размер массива: ");
+int x =Convert.ToInt32(Console.ReadLine());
+
+double[] array = new double[x];
+Random random=new Random();
+
+for(int i=0; i<array.Length; i++)
+    array[i]=random.Next(0,100);
+
+Console.WriteLine("[{0}]",string.Join(",",array));
+Sum(array);
+Console.WriteLine("[{0}]",string.Join(",",array));
