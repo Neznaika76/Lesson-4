5df1af56e7288ad4efca344827659b1a4c498927
Pavel <n9032692530@gmail.com>
2022-09-05 18:13:59 +0300

    Tasks 29

--- Tasks/Ex003/Program.cs
+++ Tasks/Ex003/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int []array = new int[8];
+Random random = new Random();
+
+for(int i=0; i<array.Length; i++)
+    array[i]=random.Next(0,100);
+
+Console.WriteLine("[{0}]",string.Join(",",array));
